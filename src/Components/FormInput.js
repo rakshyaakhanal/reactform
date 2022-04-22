8c38d6d67d80b1a59069b94190c9a3708f5df87a
Rakshya <abc@gmail.com>
2022-04-22 12:22:59 +0545

    reactform updated

---
+++ src/Components/FormInput.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './FormInput.css';
+
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocused = (e) => {
+    setFocused(true);
+  }
+
+  return (
+    <div className='formInput'>
+      <label htmlFor=''>{label}</label>
+      <input {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocused}
+        onFocus={() => inputProps.name === 'confirmPassword' && setFocused(true)}
+        focused={focused.toString()} />
+      <span>{errorMessage}</span>
+    </div>
+  )
+};
+
+export default FormInput;
